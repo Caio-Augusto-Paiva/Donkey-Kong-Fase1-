00eb5478d0a9dc8d32f89aa0e4451d394a8c4fad
fgotelip <felipegotdelga@gmail.com>
2025-11-24 21:10:36 -0300

    comentarios finais no código

--- barril.gd
+++ barril.gd
@@ -1,29 +1,29 @@
 extends RigidBody2D
 
-var velocidade_constante = 200
-var desaceleramento = velocidade_constante * 0.4
-var multiplicador_aletororio = randf_range(0.9,1.3)
+var velocidade_constante = 200 # valor base da velocidade do barril
+var desaceleramento = velocidade_constante * 0.4 # desaceleramento para mudança gradual de direção
+var multiplicador_aletororio = randf_range(0.9,1.3) # multiplicador para velocidade base -> aumentar a dificuldade do jogo
 
 func _integrate_forces(state):
 	var velocidade_atual_y = state.linear_velocity.y
 	
-	state.linear_velocity = Vector2(velocidade_constante*multiplicador_aletororio,velocidade_atual_y)
+	state.linear_velocity = Vector2(velocidade_constante*multiplicador_aletororio,velocidade_atual_y) # altera a velocidade x do barril
 
 func _on_body_entered(body):
-	if body.is_in_group("jogador"):
+	if body.is_in_group("jogador"): # caso haja colisão com jogador
 		
-		body.set_physics_process(false)
+		body.set_physics_process(false) # jogador para
 		
-		get_parent()._on_abismo_body_entered(body)
+		get_parent()._on_abismo_body_entered(body) # game over (mesmo game over do abismo)
 		
 			
 
 
 func _on_detector_inversor_area_entered(area):
-	if area.is_in_group("inversores"):
-		var velocidade_constante_aux = velocidade_constante
-		velocidade_constante = desaceleramento
-		await get_parent().criar_pausa(0.4)
-		velocidade_constante = velocidade_constante_aux * -1
-		desaceleramento *= -1
+	if area.is_in_group("inversores"): # caso haja colisão com inversor
+		var velocidade_constante_aux = velocidade_constante 
+		velocidade_constante = desaceleramento # desacelara
+		await get_parent().criar_pausa(0.4) # pausa para que o barril caia na proxima plataforma
+		velocidade_constante = velocidade_constante_aux * -1 # inverte velocidade
+		desaceleramento *= -1 # inverte desaceleramento para o proximo inversor funcionar corretamente
 	
--- level.gd
+++ level.gd
@@ -5,45 +5,40 @@ var cena_barril = preload("res://barril.tscn")
 func reiniciar_fase():
 	get_tree().reload_current_scene()
 	
-func criar_pausa(tempo : float = 1): #cria pausas no jogo para ficar mais fluido para o jogador
+func criar_pausa(tempo : float = 1):
 	await get_tree().create_timer(tempo).timeout
 
-func criar_barril():
+func criar_barril(): # cria barril no Maker colocado na mao do Kong
 	var novo_barril = cena_barril.instantiate()
 	
 	novo_barril.position = $SpawBarril.position
 	
 	add_child(novo_barril)
 
-func _ready():
+func _ready(): # inicia o level criando um barril
 	await criar_pausa(0.5)
 	criar_barril()
 
-func _on_timer_timeout():
-	var novo_barril = cena_barril.instantiate()
-	
-	novo_barril.position = $SpawBarril.position
-	
-	add_child(novo_barril)
+func _on_timer_timeout(): # quando o timer acabar spawna um novo barril
+	criar_barril()
 	
 
 func _on_objetivo_body_entered(body):
-	# Verifica se quem encostou foi o jogador
-	if body.is_in_group("jogador"):
-		print("Você ganhou, parabéns!")
+	if body.is_in_group("jogador"): # caso princesa colida com o jogador
+		print("Você ganhou, parabéns!") 
 		
 		body.set_physics_process(false)
-		await criar_pausa(2.2)	
-		# Reinicia a fase para jogar de novo
-		call_deferred("reiniciar_fase")
+		await criar_pausa(2.2)	# pausa para diregir a vitoria
+		call_deferred("reiniciar_fase") # reinicia a fase 
 
 
 func _on_abismo_body_entered(body):
-	if body.is_in_group("jogador"):
+	if body.is_in_group("jogador"): # caso jogador colida com o abismo
 		print("Game Over!")
 		
-		await criar_pausa()
+		await criar_pausa() # pausa para diregir o game over
 		
-		call_deferred("reiniciar_fase")
+		call_deferred("reiniciar_fase") # reinicia a fase
 		
-# call_deferred evita erros -> caso o godot esteja vazendo cálculos irá espera acabar os cálculos para chamar a função
+		
+# call_deferred evita erros -> caso o godot esteja fazendo cálculos irá espera acabar os cálculos para chamar a função
